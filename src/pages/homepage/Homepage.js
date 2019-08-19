03e87c6788c69499cf019229c26549392cc95800
Rituraj <kumar.shubham.learn@gmail.com>
2019-08-19 17:32:24 +0530

    finished styling menu-item component

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Homepage.scss'
+import Directory from '../../components/directory/Directory';
+
+const Homepage = () => (
+  <div className='Homepage'>
+    <Directory/>
+  </div>
+);
+
+export default Homepage;
